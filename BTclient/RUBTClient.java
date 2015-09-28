ed3a4077fc53d0d8fd8dc171c1cb3d22c3821d32
Alex <alex0906093@gmail.com>
2015-09-28 18:51:34 -0400

    first commit

---
+++ BTclient/RUBTClient.java
@@ -0,0 +1,49 @@
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.io.*;
+
+
+public class RUBTClient{
+
+	public static void main(String[] args){
+		String torrentFN;
+		String saveFN;
+		if(args.length == 2){
+			torrentFN = args[0];
+			saveFN = args[1];
+			//System.out.println("torrent file: " + torrentFN + " save file: " + saveFN);
+		}
+		else{
+			System.out.println("Invalid number of command line arguments");
+			return;
+		}
+		File torrentFile = new File(torrentFN);
+		BufferedReader reader = null;
+		try{
+			reader = new BufferedReader(new FileReader(torrentFile));
+			String encodedText = null;
+			String getLines = null;
+			while((getLines = reader.readLine()) != null){
+				encodedText = encodedText + getLines;
+			}
+			System.out.println("encoded text is :" + encodedText);
+			return;
+		} catch(FileNotFoundException e){
+			System.out.print("File not found: ");
+			e.printStackTrace();
+		} catch(IOException e){
+			e.printStackTrace();
+		} finally {
+			try{
+				if(reader != null){
+					reader.close();
+				}
+			} catch(IOException e){
+
+			}
+
+		}
+	}
+
+
+}
