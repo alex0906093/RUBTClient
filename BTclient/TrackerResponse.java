aa4fbb5d56e75f7fadd7827238eb1c8b05a0313d
Alexander J. Aguilera <aja143@rsc003.nb-labs.rutgers.edu>
2015-10-07 18:57:18 -0400

    added TrackerResponse

--- BTclient/TrackerResponse.java
+++ BTclient/TrackerResponse.java
@@ -7,10 +7,59 @@ import java.util.HashMap;
  *
  */
 public class TrackerResponse{
+    /*
+     *Fields to be received from tracker
+     */
     public String TrackerID;
     public String failureReason;
     public String failureMessage;
     public int interval;
     public int minInterval;
-
+    public int complete;
+    public int incomplete;
+    /*
+     *Keys for fields
+     */
+    public static final ByteBuffer KEY_FAILURE = ByteBuffer.wrap(new byte[]{'f', 'a', 'i','l','u', 'r', 'e', ' ', 'r', 'e', 'a', 's', 'o', 'n',});
+    public static final ByteBuffer KEY_PEERS = ByteBuffer.wrap(new byte[] {'p', 'e', 'e', 'r', 's' });
+    public static final ByteBuffer KEY_INTERVAL = ByteBuffer.wrap(new byte[] {'i', 'n', 't', 'e', 'r', 'v', 'a','l' });
+    public static final ByteBuffer KEY_MIN_INTERVAL = ByteBuffer.wrap(new byte[] { 'm', 'i', 'n', ' ', 'i', 'n', 't', 'e', 'r','v', 'a', 'l' });
+    public static final ByteBuffer KEY_COMPLETE = ByteBuffer.wrap(new byte[] {'c', 'o', 'm', 'p', 'l', 'e', 't', 'e' });
+    public static final ByteBuffer KEY_INCOMPLETE = ByteBuffer.wrap(new byte[] {'i', 'n', 'c', 'o', 'm', 'p', 'l', 'e', 't', 'e' });
+    
+    public TrackerResponse(HashMap<ByteBuffer, Object> response) throws Exception{
+        /*
+         *get all of the keys from the hash map and set them accordingly
+         *
+         */
+        if(response.containsKey()){
+            throw new Exception("Tracker Failed Key Detected");
+        }
+        if(response.containsKey(KEY_INTERVAL)){
+            this.interval = (Integer) response.get(KEY_INTERVAL);
+        }else{
+            System.out.println("No Intverval, set to 0");
+            this.interval = 0;
+        }
+        if (response.containsKey(KEY_COMPLETE))
+            this.complete = (Integer) response.get(KEY_COMPLETE);
+        else {
+            System.out.println("Warning: no complete, setting to zero");
+            this.complete = 0;
+        }
+        if (response.containsKey(KEY_INCOMPLETE))
+            this.incomplete = (Integer) response.get(KEY_INCOMPLETE);
+        else {
+            System.out.println("Warning: no incomplete, setting to zero");
+            this.incomplete = 0;
+        }
+        if (response.containsKey(KEY_MIN_INTERVAL))
+            this.minInterval = (Integer) response.get(KEY_MIN_INTERVAL);
+        else {
+            System.out.println("Warning: no minimum interval, setting to zero");
+            this.minimumInterval = 0;
+        }
+    }
+    
+    
 }
